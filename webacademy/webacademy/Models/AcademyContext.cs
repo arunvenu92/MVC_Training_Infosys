c888a29455daf1a31fe49f07edcb880160ae6513
kneerunjun <kneerunjun@outlook.com>
2015-01-20 10:57:44 +0530

    created database and connection configurations

---
+++ webacademy/webacademy/Models/AcademyContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webacademy.Models
+{
+    public class AcademyContext:DbContext
+    {
+    }
+}
