b0b4e19f8f68d3621b483a3be354c2567ca66e6e
kneerunjun <kneerunjun@outlook.com>
2015-01-20 15:50:18 +0530

    added view model and razor engine concepts

--- webacademy/webacademy/Controllers/SkillsController.cs
+++ webacademy/webacademy/Controllers/SkillsController.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using webacademy.Models;
 
 namespace webacademy.Controllers
 {
     public class SkillsController : Controller
     {
-        //
-        // GET: /Skills/
-
         public ActionResult Index()
         {
-            return View("Index");
+            vmIndex vm = new vmIndex() 
+            {
+                ExtraMessage ="We are inside the Skills controller and Index action"
+            };
+            return View("Index", vm);
         }
 
     }
---
+++ webacademy/webacademy/Models/vmIndex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webacademy.Models
+{
+    public class vmIndex
+    {
+        public string ExtraMessage { get; set; }
+    }
+}
